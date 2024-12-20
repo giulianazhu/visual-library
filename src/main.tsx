0e8c2278a02c49145634ecceca90f1ff7f682e3c
Giuliana Zhu <135511633+giulianazhu@users.noreply.github.com>
2024-12-20 16:52:54 +0100

    remove top level await

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@styles/index.scss'
 import App from './App.tsx'
+import initLanguage from 'core/locale/i18n.js'
 // initializing i18n
-import 'core/locale/i18n'
+// import 'core/locale/i18n'
+const onLoad = async () => {
+  await initLanguage()
+  return createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+}
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+onLoad()
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -2,14 +2,16 @@ import { Button } from 'antd'
 import style from './styles.module.scss'
 import { useThemeContext } from 'core/providers/theme'
 import { Theme } from '@app-types/enums'
+import { useTranslation } from 'react-i18next'
 
 function Dashboard() {
   const { theme, setTheme } = useThemeContext()
+  const { t } = useTranslation()
   return (
     <>
       <div className={style['dashboard']}>Dashboard</div>
       <Button type="primary" onClick={() => setTheme(theme === Theme.Dark ? Theme.Light : Theme.Dark)}>
-        Hello{' '}
+        {t('translation.welcome')}
       </Button>
     </>
   )
