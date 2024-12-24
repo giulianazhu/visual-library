708be2b3c368b2456f123cd092101d9e9ff179da
Giuliana Zhu <135511633+giulianazhu@users.noreply.github.com>
2024-12-24 10:48:21 +0100

    template + config fix

---
+++ src/shared/ui/button/Button.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Button from './Button'
+
+describe('Button Component', () => {
+  it('renders correctly', () => {
+    render(<Button prop="string" />)
+    const el = screen.getByText('')
+    expect(el).toBeInTheDocument()
+  })
+
+  // it('handles user interaction', async () => {
+
+  // const handleEvent = vi.fn()
+  // render(<Button prop="string" />)
+
+  // await userEvent.click(screen.getByText('Click Me'))
+  // expect(handleEvent).toHaveBeenCalledTimes(1)
+  //})
+})
